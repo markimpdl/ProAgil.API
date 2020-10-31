fe231f8d6a9f3bb5e84565d55eca403ba988efb4
Marcos Ortolani <marcos.ortolani@hotmail.com>
2020-10-30 21:33:04 -0300

    Retornando valores de exemplo por GET

--- ProAgil.API/Controllers/ValuesController.cs
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Model;
 
 namespace ProAgil.API.Controllers
 {
@@ -12,9 +13,27 @@ namespace ProAgil.API.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new Evento[] { 
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular e .Net Core",
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular e suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "3º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                },
+
+             };
         }
 
         // GET api/values/5
