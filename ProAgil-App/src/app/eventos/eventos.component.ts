dd8e7adde5173c06dbbb1fb602328240c9061b06
Marcos Ortolani <marcos.ortolani@hotmail.com>
2020-11-01 21:15:38 -0300

    Adicionado filtro por tema

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,25 +8,44 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+  _filtroLista: string;
+
+  get filtroLista(): string{
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+
+  eventosFiltrados: any = [];
   eventos: any = [];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  filtroLista = '';
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit(): any{
     this.getEventos();
   }
 
-  alternarImagem(){
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  alternarImagem(): any{
     this.mostrarImagem = !this.mostrarImagem;
   }
 
-  getEventos(){
+  getEventos(): any{
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.eventos = response;
+      console.log(response);
     }, error => {
       console.log(error);
     });
