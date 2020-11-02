65ee64b061dc47e91f36b22b283fa3de2346e7a0
Marcos Ortolani <marcos.ortolani@hotmail.com>
2020-11-01 23:33:57 -0300

    Projeto Reorganizado em Camadas e novas classes

--- ProAgil.API/Controllers/ValuesController.cs
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ProAgil.API.Data;
-using ProAgil.API.Model;
+using ProAgil.Repository;
+
 
 namespace ProAgil.API.Controllers
 {
@@ -14,8 +14,8 @@ namespace ProAgil.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        public readonly DataContext _context;
-        public ValuesController(DataContext context)
+        public readonly ProAgilContext _context;
+        public ValuesController(ProAgilContext context)
         {
             _context = context;
         }
@@ -37,35 +37,7 @@ namespace ProAgil.API.Controllers
         }
 
 
-        /* Request usando Action Result substituido por  Async IAction Result
         
-        public ActionResult<IEnumerable<Evento>> Get()
-        {
-            return _context.Eventos.ToList();
-            /* 
-            //Exemplo de retorno 
-            return new Evento[] {
-                new Evento() {
-                    EventoId = 1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento() {
-                    EventoId = 2,
-                    Tema = "Angular e suas Novidades",
-                    Local = "São Paulo",
-                    Lote = "3º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-
-             }; 
-
-
-        } */
 
         // GET api/values/5
         [HttpGet("{id}")]
@@ -74,7 +46,7 @@ namespace ProAgil.API.Controllers
         {
             try
             {
-                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
                 return Ok(results);
             }
             catch (System.Exception)
@@ -84,13 +56,7 @@ namespace ProAgil.API.Controllers
             
         }
         
-        /* substituindo request por id por async 
-        public ActionResult<Evento> Get(int id)
-        {
-            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
-        }
-        */
-
+    
 
 
         // POST api/values
